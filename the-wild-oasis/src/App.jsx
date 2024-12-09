6b42e7f66d2e9a0156150418300f388d2783f5f0
Anupam Gupta <64581827+anupam-dosar@users.noreply.github.com>
2024-12-09 15:32:43 +0530

    styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(-shadow-sm);
+  cursor: pointer;
+`;
+
 function App() {
-  return <div>wild oasis</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>The Wild Oasis</H1>
+        <Button>Check In</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
