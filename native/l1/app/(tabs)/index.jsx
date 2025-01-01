c866bbbd6e7aa298856ceccb71899e3f9b9d70bd
Anupam Gupta <64581827+anupam-dosar@users.noreply.github.com>
2025-01-01 15:36:31 +0530

    native l1 init

---
+++ native/l1/app/(tabs)/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
+
+import icedCoffeeImg from "@/assets/images/iced-coffee.png";
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={icedCoffeeImg} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default app;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  image: {
+    flrex: 1,
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+});
